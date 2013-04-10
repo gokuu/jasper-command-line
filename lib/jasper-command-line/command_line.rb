3e9b8eecad5cc9bea589bd533a89aaf8d3812955
Pedro Rodrigues <pedro@bbde.org>
2013-04-10 21:36:39 +0100

    Parse the command line using OptionParser

--- lib/jasper-command-line/command_line.rb
+++ lib/jasper-command-line/command_line.rb
@@ -4,44 +4,20 @@ module JasperCommandLine
       begin
         options = parse_arguments(args)
 
-        if !options
-          puts "Usage:"
-          puts ""
-          puts "jasper-command-line [options]"
-          puts ""
-          puts "Options:"
-          puts ""
-          puts "--jasper /path/to/file     The .jasper file to load (if one doesn't exist, it is"
-          puts "                           compiled from the .jrxml file with the same name and"
-          puts "                           on the same location)"
-          puts "--data-file /path/to/file  The .xml file to load the data from"
-          puts "--copies number            The number of copies to generate"
-          puts "--locale locale            The locale to use in the report (in the format xx-YY)"
-          puts "--param key=value          Adds the parameter with name key with the value value"
-          puts "                           (can be defined multiple times)"
-          puts ""
-          puts "Digital signature options:"
-          puts "--sign-key-file /path/to/file  The location of the PKCS12 file to"
-          puts "                               digitally sign the PDF with"
-          puts "--sign-password password       The password for the PKCS12 file"
-          puts "--sign-location location       The location of the signature"
-          puts "--sign-reason reason           The reason for signing the PDF"
-        else
-          if options[:jasper_file]
-            jasper_file = options.delete :jasper_file
-            data = options.delete :data
-            params = options.delete :params
+        if options[:jasper_file]
+          jasper_file = options.delete :jasper_file
+          data = options.delete :data
+          params = options.delete :params
 
-            if options[:signature]
-              if options[:signature][:key_file] && !options[:signature][:password]
-                raise ArgumentError.new("Password not supplied for certificate")
-              end
+          if options[:signature]
+            if options[:signature][:key_file] && !options[:signature][:password]
+              raise ArgumentError.new("Password not supplied for certificate")
             end
-
-            puts JasperCommandLine::Jasper::render_pdf(jasper_file, data, params, options)
           end
+
+          puts JasperCommandLine::Jasper::render_pdf(jasper_file, data, params, options)
         end
-      rescue ArgumentError => e
+      rescue OptionParser::InvalidOption => e
         puts "Error: #{e.message}"
       rescue => e
         puts "Error: #{e.message}"
@@ -52,92 +28,94 @@ module JasperCommandLine
     private
 
       def parse_arguments(arguments)
+        require 'optparse'
+        require 'optparse/time'
+        require 'ostruct'
+
         data = {
           :params => {}
         }
 
-        return false unless arguments.any?
+        # Default to the help message
+        arguments << '--help' if arguments.empty?
 
-        i = 0
-
-        while i < arguments.length
-          argument = arguments[i]
-
-          case get_option(argument)
-            when 'data'
-              i = get_option_data(arguments, i) do |argument_data|
-                raise ArgumentError.new("Data not found") if get_option(argument_data)
-                data[:data] = argument_data
-              end
-
-            when 'data-file'
-              i = get_option_data(arguments, i) do |argument_data|
-                raise ArgumentError.new("Data file not found: #{argument_data}") unless File.exists?(argument_data)
-                data[:data] = File.read argument_data
-              end
-
-            when 'param'
-              i = get_option_data(arguments, i) do |argument_data|
-                raise ArgumentError.new("Invalid param format: #{argument_data}") unless argument_data =~ /(.*?)=(.*)/
-                data[:params][$1] = $2
-              end
-
-            when 'jasper'
-              i = get_option_data(arguments, i) do |argument_data|
-                raise ArgumentError.new("File not found: #{argument_data}") unless File.exists?(argument_data) || File.exists?(argument_data.gsub(/\.jasper$/, '.jrxml'))
-                data[:jasper_file] = argument_data
-              end
-
-            when 'copies'
-              i = get_option_data(arguments, i) do |argument_data|
-                raise ArgumentError.new("Invalid number of copies: #{argument_data}") unless argument_data =~ /^[1-9][0-9]*$/
-                data[:copies] = argument_data.to_i
-
-                raise ArgumentError.new("Ghostscript isn't available. Merging is not possible") if data[:copies] > 1 && !JasperCommandLine.ghostscript_available?
-              end
-
-            when 'locale'
-              i = get_option_data(arguments, i) do |argument_data|
-                raise ArgumentError.new("Invalid locale: #{argument_data}") unless argument_data =~ /^[a-z]{2}-[A-Za-z0-9]{2,3}$/
-                data[:locale] = {
-                  :language => argument_data.strip.split('-').first,
-                  :sub_language => argument_data.strip.split('-').last
-                }
-              end
-
-            when 'sign-key-file'
-              # Sign document with file
-              i = get_option_data(arguments, i) do |argument_data|
-                raise ArgumentError.new("Signature key file not found: #{argument_data}") unless File.exists?(argument_data)
-                data[:signature] ||= {}
-                data[:signature][:key_file] = argument_data
-              end
-
-            when 'sign-location'
-              # Location to set on the signature
-              i = get_option_data(arguments, i) do |argument_data|
-                data[:signature] ||= {}
-                data[:signature][:location] = argument_data
-              end
-
-            when 'sign-password'
-              # Password to open the signature key file
-              i = get_option_data(arguments, i) do |argument_data|
-                data[:signature] ||= {}
-                data[:signature][:password] = argument_data
-              end
-
-            when 'sign-reason'
-              # Reason to set on the signature
-              i = get_option_data(arguments, i) do |argument_data|
-                data[:signature] ||= {}
-                data[:signature][:reason] = argument_data
-              end
-
-            else
-              i += 1
-          end
+        opts = OptionParser.new do |opts|
+          opts.banner = "Usage: jasper-command-line [options]"
         end
+
+        opts.separator ""
+        opts.separator "Options:"
+
+        opts.on('-j', '--jasper file', "The .jasper file to load (if one doesn't exist, it is compiled from the .jrxml file with the same name and on the same location)") do |file|
+          raise ArgumentError.new("File not found: #{file}") unless File.exists?(file) || File.exists?(file.gsub(/\.jasper$/, '.jrxml'))
+          data[:jasper_file] = file
+        end
+
+        opts.on('-d', '--data-file file', "The .xml file to load the data from") do |file|
+          raise ArgumentError.new("Data file not found: #{file}") unless File.exists?(file)
+          data[:data] = File.read file
+        end
+
+        opts.on('-c', '--copies number', Integer, "The number of copies to generate") do |i|
+          data[:copies] = i
+          raise ArgumentError.new("Ghostscript isn't available. Merging is not possible") if data[:copies] > 1 && !JasperCommandLine.ghostscript_available?
+        end
+
+        opts.on('-l', '--locale locale', "The locale to use in the report (in the format xx-YY)") do |locale|
+          raise ArgumentError.new("Invalid locale format: #{locale}") unless locale =~ /([a-z]{2})-([A-Z]{2})/
+          data[:locale] = { language: $1, sub_language: $2 }
+        end
+
+        opts.on('--param name=value', "Adds the parameter with name key with the value value (can be defined multiple times)") do |parameter|
+          raise ArgumentError.new("Invalid param format: #{parameter}") unless parameter =~ /([^\s]+)=(.*)/
+          data[:params][$1] = $2
+        end
+
+        opts.separator ""
+        opts.separator "Digital signature options:"
+
+        opts.on('--sign-key-file file', "The location of the PKCS12 file to digitally sign the PDF with") do |file|
+          # Sign document with file
+          raise ArgumentError.new("Signature key file not found: #{file}") unless File.exists?(file)
+          data[:signature] ||= {}
+          data[:signature][:key_file] = file
+        end
+
+        opts.on('--sign-location location', "The location data for the signature") do |location|
+          # Location to set on the signature
+          data[:signature] ||= {}
+          data[:signature][:location] = location
+        end
+
+        opts.on('--sign-password password', "The password for the PKCS12 file") do |password|
+          # Password to open the signature key file
+          data[:signature] ||= {}
+          data[:signature][:password] = password
+        end
+
+        opts.on('--sign-reason reason', "The reason for signing the PDF") do |reason|
+          # Reason to set on the signature
+          data[:signature] ||= {}
+          data[:signature][:reason] = reason
+        end
+
+        opts.separator ""
+        opts.separator "Common options:"
+
+        # No argument, shows at tail.  This will print an options summary.
+        # Try it and see!
+        opts.on_tail("-h", "--help", "Show this message") do
+          puts opts
+          exit
+        end
+
+        # Another typical switch to print the version.
+        opts.on_tail("--version", "Show version") do
+          puts JasperCommandLine::VERSION
+          exit
+        end
+
+        opts.parse! arguments
 
         return data
       end
